9ce902777ecd6ada03cb5167b3eb4abff641c944
martins86 <martins86@gmail.com>
2021-05-08 19:26:32 +0000

    Criando Rotas

---
+++ servidor-api/routes/api.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+var PersonController = require("./../controllers/PersonController");
+var ProductController = require("./../controllers/ProductController");
+
+router.get('/people', PersonController.all);
+router.get('/products', ProductController.all);
+
+module.exports = router;
