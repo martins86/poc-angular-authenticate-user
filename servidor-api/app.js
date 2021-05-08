1b81df69c1402eb3e0eed5876d1d360f799443b1
martins86 <martins86@gmail.com>
2021-05-08 19:27:03 +0000

    Chamando rotas

--- servidor-api/app.js
+++ servidor-api/app.js
@@ -3,6 +3,7 @@ var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
 var cors = require("cors");
 var app = express();
+var api = require("./routes/api");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
@@ -12,6 +13,8 @@ mongoose.connect(
     'mongodb://localhost:27017/authentication',
     { useNewUrlParser: true }
 );
+
+app.use('/api', api);
 
 app.use(
     function(req, res, next) {
