5ab4ae0c196d14a64daa9e3ff7a0165c4c1033ba
martins86 <martins86@gmail.com>
2021-05-08 19:26:19 +0000

    Criando controllers

---
+++ servidor-api/controllers/PersonController.js
@@ -0,0 +1,16 @@
+var PersonModel = require("./../models/PersonModel");
+
+var Controller = {
+    all: function(req, res) {
+        PersonModel.find({}).lean().exec(
+            function(error, people) {
+                if (error) {
+                    return res.json(error);
+                }
+                return res.json(people);
+            }
+        );
+    },
+};
+
+module.exports = Controller;
---
+++ servidor-api/controllers/ProductController.js
@@ -0,0 +1,16 @@
+var ProductModel = require("./../models/ProductModel");
+
+var Controller = {
+    all: function(req, res) {
+        ProductModel.find({}).lean().exec(
+            function(error, products) {
+                if (error) {
+                    return res.json(error);
+                }
+                return res.json(products);
+            }
+        );
+    },
+};
+
+module.exports = Controller;
