fe1727876c4ea9a6fdce7fc717781825118dea69
martins86 <martins86@gmail.com>
2021-05-08 19:01:21 +0000

    Configurando api

---
+++ servidor-api/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(cors());
+
+mongoose.connect(
+    'mongodb://localhost:27017/authentication',
+    { useNewUrlParser: true }
+);
+
+app.use(
+    function(req, res, next) {
+        res.status(404).send('Not found');
+    }
+);
+
+app.listen(3000);
