f164e5ca5fe08879b276e297e078ae6be7eae828
martins86 <martins86@gmail.com>
2021-05-10 01:24:08 +0000

    adicionando service

---
+++ aplicacao-web/src/app/pages/products/shared/services/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { DataProduct } from './../models/data-product.model';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+readonly urlApi = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) { }
+
+  getPeople(): Observable<DataProduct[]> {
+    return this.http.get<DataProduct[]>(`${this.urlApi}/product`)
+      .pipe(
+        catchError(
+          (e) => {
+            console.error(e);
+            return throwError(e);
+          }
+        )
+      );
+  }
+}
