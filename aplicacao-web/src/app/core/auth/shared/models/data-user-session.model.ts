b9e1b58197a394152f104d617195128618d20d0e
martins86 <martins86@gmail.com>
2021-05-12 00:58:12 +0000

    Adicionando model user

---
+++ aplicacao-web/src/app/core/auth/shared/models/data-user-session.model.ts
@@ -0,0 +1,14 @@
+export class DataUserSession {
+    constructor(
+        public firstname: string,
+        public lastname: string,
+        public email: string,
+        public password: string,
+        public phone: string,
+        public mobilephone: string,
+        public address: string,
+        public city: string,
+        public state: string,
+        public id?: number,
+    ) {}
+}
