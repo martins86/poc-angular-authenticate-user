e8ba7144261fe27580d45396ef65890b59bbf961
martins86 <martins86@gmail.com>
2021-05-12 00:57:19 +0000

    Adicionando material

--- aplicacao-web/src/app/pages/people/people.module.ts
+++ aplicacao-web/src/app/pages/people/people.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-const materialModule = [MatListModule, MatDividerModule, MatIconModule];
+const materialModule = [MatListModule, MatDividerModule, MatIconModule, MatProgressSpinnerModule];
 
 import { PeopleRoutingModule } from './people-routing.module';
 import { PeopleComponent } from './people.component';
--- aplicacao-web/src/app/pages/products/products.module.ts
+++ aplicacao-web/src/app/pages/products/products.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-const materialModule = [MatListModule, MatDividerModule, MatIconModule];
+const materialModule = [MatListModule, MatDividerModule, MatIconModule, MatProgressSpinnerModule];
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
